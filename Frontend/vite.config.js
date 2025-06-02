a86b40589679e229ae599d70b8e855087ef4b98a
Raviteja <ravitejaseguri@gmail.com>
2025-06-02 14:20:41 +0530

    Remove base href from index.html and simplify Vite configuration by removing server and preview settings

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,19 +7,4 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 5173,
-    strictPort: true,
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 5173,
-    strictPort: true,
-    allowedHosts: [
-      'online-auction-platform-front.onrender.com',
-      'localhost',
-      '127.0.0.1'
-    ]
-  }
 })
